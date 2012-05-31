aa3cb1c1b479cb43e5fa65d4f96c0ebef70a52f4
Kyle Wilcox <wilcox.kyle@gmail.com>
2012-05-30 21:01:34 -0400

    Update dependency build script

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 mkdir tmp-pip
 pip install -r requirements.txt -t tmp-pip
-zip -r WEB-INF/lib-python/site-packages.zip tmp-pip/*
-rm -rf tmp-pip
+cd tmp-pip
+rm -f site-packages.zip
+zip -r site-packages.zip *
+mv -f site-packages.zip ../WEB-INF/lib-python/site-packages.zip
+
+# zip -r -9 jython-lib.zip /home/dev/.pythonz/pythons/Jython-2.5.2/Lib/*
+# mv -f jython-lib.zip ../WEB-INF/lib-python/jython-lib.zip
