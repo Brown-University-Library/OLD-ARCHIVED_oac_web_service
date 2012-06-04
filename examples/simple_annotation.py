e50e7812890648f030ec5c9e12d1b4d4eff4aca1
Kyle Wilcox <wilcox.kyle@gmail.com>
2012-06-03 21:27:19 -0400

    Whitespace cleanup

--- examples/simple_annotation.py
+++ examples/simple_annotation.py
@@ -4,15 +4,15 @@ import urllib
 import urllib2
 post_url = "http://daxdev.services.brown.edu:8081/oac_web_service/annotate"
 params = { 
-    "source_uri"    : "test:1#xpointer('/foo')",
-    "body_content"  : "<TEI><body>text body</body></TEI>",
-    "body_mimetype" : "text/xml",
-    "dc_title"      : "Open Annotation Collaboration Annotation object (A-1)"
+    "source_uri"            : "test:1#xpointer('/foo')",
+    "body_content"          : "<TEI><body>text body</body></TEI>",
+    "body_mimetype"         : "text/xml",
+    "dc_title"              : "Open Annotation Collaboration Annotation object (A-1)"
 }
 encoded_data = urllib.urlencode( params )
 request = urllib2.Request( post_url, encoded_data )
 response = urllib2.urlopen( request )
-
+print response.read()
 
 
 """
