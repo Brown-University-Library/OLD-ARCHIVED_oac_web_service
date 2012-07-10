8e01ae54a34a1e19ecd90740bded9bb887c25f8d
Kyle Wilcox <wilcox.kyle@gmail.com>
2012-07-10 16:51:28 -0400

    Messaging working.

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 sudo /etc/init.d/tomcat6 stop
-sudo /etc/init.d/tomcat7 stop
+#sudo /etc/init.d/tomcat7 stop
+
+sleep 6
 
 sudo rm -rf /var/www/tomcat/apache-tomcat-6.0.35/webapps/oac_web_service*
 sudo rm -rf /var/www/tomcat/apache-tomcat-7.0.27/webapps/oac_web_service*
 
-jar cvf oac_web_service.war WEB-INF META-INF oac_web_service.py oac_web_service README
+jar cvf oac_web_service.war WEB-INF oac_web_service.py oac_web_service README
 sudo cp oac_web_service.war /var/www/tomcat/apache-tomcat-6.0.35/webapps/
 sudo cp oac_web_service.war /var/www/tomcat/apache-tomcat-7.0.27/webapps/
 
 sudo /etc/init.d/tomcat6 start
-sudo /etc/init.d/tomcat7 start
+#sudo /etc/init.d/tomcat7 start
+
+sleep 6
+
+sudo touch /var/www/tomcat/apache-tomcat-6.0.35/webapps/fedora/WEB-INF/web.xml
