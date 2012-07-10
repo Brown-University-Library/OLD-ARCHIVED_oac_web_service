f6b3755dd82ba971d93de30da7f46adb9c560099
Kyle Wilcox <wilcox.kyle@gmail.com>
2012-07-10 00:50:18 -0400

    WIP: messaging

--- deploy_app.sh
+++ deploy_app.sh
@@ -5,7 +5,7 @@ sudo /etc/init.d/tomcat7 stop
 sudo rm -rf /var/www/tomcat/apache-tomcat-6.0.35/webapps/oac_web_service*
 sudo rm -rf /var/www/tomcat/apache-tomcat-7.0.27/webapps/oac_web_service*
 
-jar cvf oac_web_service.war WEB-INF oac_web_service.py oac_web_service README
+jar cvf oac_web_service.war WEB-INF META-INF oac_web_service.py oac_web_service README
 sudo cp oac_web_service.war /var/www/tomcat/apache-tomcat-6.0.35/webapps/
 sudo cp oac_web_service.war /var/www/tomcat/apache-tomcat-7.0.27/webapps/
 
