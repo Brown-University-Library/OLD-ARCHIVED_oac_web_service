bc27252714cde58345e65bbce855bdf8afc07397
Kyle Wilcox <wilcox.kyle@gmail.com>
2012-07-12 01:08:10 -0400

    Ability to serialize more than one PID at a time

--- examples/serialize.py
+++ examples/serialize.py
@@ -4,8 +4,8 @@ import urllib
 import urllib2
 get_url = "http://daxdev.services.brown.edu:8081/oac_web_service/show"
 params = { 
-  "pid"		: "changeme:9",
-  "format"	: "json"
+  "pid"     : "changeme:9,changeme:166",
+  "format"  : "json"
 }
 args = urllib.urlencode( params )
 request = urllib2.Request( get_url + "?" + args)
